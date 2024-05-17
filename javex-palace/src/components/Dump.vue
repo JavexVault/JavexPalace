d9bf7a2fa55ffcd4e1b25b149e986fdb6983302a
Bruno César <bcgmeireles@gmail.com>
2024-05-17 17:43:12 -0300

    feat: implements overlay in dump images

--- javex-palace/src/components/Dump.vue
+++ javex-palace/src/components/Dump.vue
@@ -1,51 +1,56 @@
 <template>
     <div class="dump">
-       <div id="div1"></div>
-
-       <div id="div2"></div>
-
-       <div id="div3"></div>
-
-       <div id="div4"></div>
+        <div class="img"><div class="bg"></div></div>
+        <div class="img"><div class="bg"></div></div>
+        <div class="img"><div class="bg"></div></div>
+        <div class="img"><div class="bg"></div></div>
     </div>
 </template>
 
 <style scoped>
-
-    #div1{
-        background-image: url("../assets/images/1.jpg");
-    }
-    #div2{
-        background-image: url("../assets/images/2.jpg");
-    }
-    #div3{
-        background-image: url("../assets/images/3.jpg");
-    }
-    #div4{
-        background-image: url("../assets/images/montanha.jpg");
-    }
-
-
     .dump{
         display: flex;
-        flex-grow: 1;
         width: 100%;
-        background-color: black;
+        height: 23em;
+        justify-content: center;
     }
-
-    div{       
-        height: 25em;
-        flex-grow: 1;
-        transition:.5s ease-in-out;
+    .img {
+        width: 100%;
+        height: 100%;
+        filter: blur(2px 0);
+        background: url(../assets/images/1.jpg);
+        background-position: center;
         background-size: cover;
-        background-position: center center;
-        background-color: black;
-        border-right: black 1px solid;
+        transition: width .3s ease-in-out;
     }
-
-    div:hover{
-        opacity: 1;
-        flex-grow:3;
+    .img:nth-child(2) {
+        background: url(../assets/images/2.jpg);
+        background-position: center;
+        background-size: cover;
+    }
+    .img:nth-child(3) {
+        background: url(../assets/images/3.jpg);
+        background-position: center;
+        background-size: cover;
+    }
+    .img:nth-child(4) {
+        background: url(../assets/images/montanha.jpg);
+        background-position: center;
+        background-size: cover;
+    }
+    .bg {
+        width: 100%;
+        height: 100%;
+        opacity: .7;
+        background-color: black;
+        transition: opacity .3s ease-in-out;
+    }
+    .img:hover {
+        width: 175%;
+        z-index: 2;
+        .bg {
+            opacity: 0;
+        }
     }
 
     @media screen and (max-width: 400px) {
